97ae2b90869583daee0518498b355c9b3248bc27
JuanDa2006 <86136478+JuanDa2006@users.noreply.github.com>
2024-01-09 09:49:38 +0100

    Up

--- WebSite/src/components/header.jsx
+++ WebSite/src/components/header.jsx
@@ -1,16 +1,13 @@
-export default function Header(){
-    return(
+export default function Header() {
+    return (
         <>
             <header>
                 <div className="header-text">
                     <h1>Genios Web</h1>
                     <p>Navegando a tu Éxito Digital</p>
-                    
                 </div>
+                <div className="header-img"></div>
                 
-                <div className="header-img">
-                    
-                </div>
             </header>
         </>
     );
