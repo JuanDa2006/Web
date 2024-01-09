44caa6210d0149d9848a4b55d0c118a0ef7fe3ac
JuanDa2006 <86136478+JuanDa2006@users.noreply.github.com>
2024-01-09 09:50:14 +0100

    Update header.jsx

--- WebSite/src/components/header.jsx
+++ WebSite/src/components/header.jsx
@@ -6,7 +6,9 @@ export default function Header() {
                     <h1>Genios Web</h1>
                     <p>Navegando a tu Éxito Digital</p>
                 </div>
-                <div className="header-img"></div>
+                <div className="header-img">
+                    <img src="https://raw.githubusercontent.com/JuanDa2006/Web/97ae2b90869583daee0518498b355c9b3248bc27/WebSite/src/assets/img/img2.svg?token=AURFNHUZ6YVNTZG35IBXJLDFTUE2M" alt="" />
+                </div>
                 
             </header>
         </>
