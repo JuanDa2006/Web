c36f8c0922331782f4a6aa0297dcad578eb2c02d
JuanDa2006 <86136478+JuanDa2006@users.noreply.github.com>
2024-01-09 10:08:10 +0100

    Up

--- WebSite/src/components/header.jsx
+++ WebSite/src/components/header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
                     <p>Navegando a tu Éxito Digital</p>
                 </div>
                 <div className="header-img">
-                    <img src="https://raw.githubusercontent.com/JuanDa2006/Web/97ae2b90869583daee0518498b355c9b3248bc27/WebSite/src/assets/img/img2.svg?token=AURFNHUZ6YVNTZG35IBXJLDFTUE2M" alt="" />
+                    <img src="../assets/img/img2.png" alt="" />
                 </div>
                 
             </header>
