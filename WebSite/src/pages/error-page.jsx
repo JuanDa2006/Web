e183a51da5cf13999444460d968fd3b1fc01a079
JuanDa2006 <86136478+JuanDa2006@users.noreply.github.com>
2024-01-18 13:32:37 +0100

    Up

--- WebSite/src/pages/error-page.jsx
+++ WebSite/src/pages/error-page.jsx
@@ -9,10 +9,14 @@ export default function ErrorPage() {
             <div className="container">
                 <h1>Error!!!</h1>
                 <p>Disculpas, ha ocurrido un error</p>
-                <a href="/">Volver</a>
                 <p>
                     <i>{error.statusText || error.message}</i>
                 </p>
+                <div className="back">
+                    <a href="/">
+                        Volver
+                    </a>
+                </div>
             </div>
         </div>
     );
