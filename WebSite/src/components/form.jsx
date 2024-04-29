4041bb44392c0520711095b55761a6a37b71624b
JuanDa2006 <86136478+JuanDa2006@users.noreply.github.com>
2024-04-29 08:18:41 +0200

    Up

---
+++ WebSite/src/components/form.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+
+const Form = () => {
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [message, setMessage] = useState();
+    const [label, setLabel] = useState('Enviar');
+
+    const send = () => {
+        if (name && email && message) {
+            const content = {
+                content: 'Mensaje',
+                embeds: [
+                    {
+                        title: 'Nombre: ' + name,
+                        email: 'Correo electrinico: ' + email,
+                        message: message,
+                        footer: {
+                            text: 'Gracias',
+                        },
+                    },
+                ],
+            };
+
+            fetch(
+                'https://discord.com/api/webhooks/1233897971620515951/dMpWnkvyPwwbTpteIdlIsqPgAavbks9TYOM5lDJDIGK5JbK-xXgEyWcLZ6xnSwumOcxq',
+                {
+                    method: 'POST',
+                    body: JSON.stringify(content),
+                    headers: {
+                        'Content-Type': 'aplication/json',
+                    },
+                }
+            ).then((res) => {
+                setName('');
+                setEmail('');
+                setMessage('');
+                setLabel('Error');
+            });
+        } else {
+            setLabel('Intentar de nuevo');
+        }
+    };
+
+    return (
+        <>
+            <h1>Contacto</h1>
+            <form id='contact-form' action='' method='post'>
+                <div className='form-item'>
+                    <input
+                        type='text'
+                        name='name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder='Nombre'
+                    />
+                </div>
+                <div className='form-item'>
+                    <input
+                        id='email'
+                        name='email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder='name@example.com'
+                    />
+                </div>
+                <div className='form-textarea'>
+                    <textarea
+                        type='text'
+                        name='message'
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        placeholder='Mensaje'
+                    />
+                </div>
+                <input type='hidden' name='form_submission' value='1' />
+                <button type='submit' class='submit-button' onClick={send}>
+                    <p>
+                        <span class='material-symbols-outlined'>send</span>
+                        {label}
+                    </p>
+                </button>
+            </form>
+        </>
+    );
+};
+
+export default Form;
--- WebSite/src/pages/contact.jsx
+++ WebSite/src/pages/contact.jsx
@@ -1,6 +1,7 @@
-import React, {Fragment, useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Nav from '../components/nav';
 import Footer from '../components/footer';
+import Form from '../components/form';
 
 export default function Contact() {
     useEffect(() => {
@@ -16,40 +17,6 @@ export default function Contact() {
                 </main>
                 <Footer />
             </section>
-        </>
-    );
-}
-
-const Form = () => {
-    return (
-        <>
-            <h1>Contacto</h1>
-            <form id='contact-form' action='' method='post'>
-                <div className='form-item'>
-                    <input type='text' name='name' placeholder='Nombre' />
-                </div>
-                <div className='form-item'>
-                    <input
-                        id='email'
-                        name='email'
-                        placeholder='name@example.com'
-                    />
-                </div>
-                <div className='form-textarea'>
-                    <textarea
-                        type='text'
-                        name='message'
-                        placeholder='Mensaje'
-                    />
-                </div>
-                <input type='hidden' name='form_submission' value='1' />
-                <button type='submit' class='submit-button'>
-                    <p>
-                        <span class='material-symbols-outlined'>send</span>
-                        Enviar
-                    </p>
-                </button>
-            </form>
         </>
     );
 }
