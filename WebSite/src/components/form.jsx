bd0d4c1ff635b9c72013fb837f24a6fe08ae0374
JuanDa2006 <86136478+JuanDa2006@users.noreply.github.com>
2024-04-30 09:52:37 +0200

    Up

--- WebSite/src/components/form.jsx
+++ WebSite/src/components/form.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import {useState} from 'react';
 
 const Form = () => {
     const [name, setName] = useState();
     const [email, setEmail] = useState();
     const [message, setMessage] = useState();
+    const [formulario, setFormulario] = useState();
+    const [gracias, setGracias] = useState('hidden');
     const [label, setLabel] = useState('Enviar');
 
     const send = () => {
@@ -35,6 +37,8 @@ const Form = () => {
                 setName('');
                 setEmail('');
                 setMessage('');
+                setFormulario('hidden');
+                setGracias('show');
                 setLabel('hidden');
             });
         } else {
@@ -45,45 +49,47 @@ const Form = () => {
     return (
         <>
             <h1>Contacto</h1>
-            <div className="formulario">
-                
+            <div className={formulario}>
+                <form id='contact-form' action='' method='post'>
+                    <div className='form-item'>
+                        <input
+                            type='text'
+                            name='name'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder='Nombre'
+                        />
+                    </div>
+                    <div className='form-item'>
+                        <input
+                            id='email'
+                            name='email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder='name@example.com'
+                        />
+                    </div>
+                    <div className='form-textarea'>
+                        <textarea
+                            type='text'
+                            name='message'
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            placeholder='Mensaje'
+                        />
+                    </div>
+                    <input type='hidden' name='form_submission' value='1' />
+                    <button type='submit' class='submit-button' onClick={send}>
+                        <p>
+                            <span class='material-symbols-outlined'>send</span>
+                            {label}
+                        </p>
+                    </button>
+                </form>
             </div>
-            <form id='contact-form' action='' method='post'>
-                <div className='form-item'>
-                    <input
-                        type='text'
-                        name='name'
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        placeholder='Nombre'
-                    />
-                </div>
-                <div className='form-item'>
-                    <input
-                        id='email'
-                        name='email'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder='name@example.com'
-                    />
-                </div>
-                <div className='form-textarea'>
-                    <textarea
-                        type='text'
-                        name='message'
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        placeholder='Mensaje'
-                    />
-                </div>
-                <input type='hidden' name='form_submission' value='1' />
-                <button type='submit' class='submit-button' onClick={send}>
-                    <p>
-                        <span class='material-symbols-outlined'>send</span>
-                        {label}
-                    </p>
-                </button>
-            </form>
+            <div className={gracias}>
+                <p>Enviado, gracias</p>
+            </div>
         </>
     );
 };
