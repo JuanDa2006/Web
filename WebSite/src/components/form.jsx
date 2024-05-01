e1f5a31dcd022538cebbb86606ab28685ab446ce
JuanDa2006 <86136478+JuanDa2006@users.noreply.github.com>
2024-05-01 19:18:19 +0200

    Up

--- WebSite/src/components/form.jsx
+++ WebSite/src/components/form.jsx
@@ -1,40 +1,60 @@
 import { useRef } from 'react';
-import emailjs, { send } from '@emailjs/browser';
+import emailjs, { sendForm } from '@emailjs/browser';
+
+const SERVICE_ID = 'service_g35rpma';
+const TEMPLATE_ID = 'template_jje55am';
+const PUBLIC_KEY = '23VVPNHkPWtgXl65Q';
 
 const Form = () => {
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY).then(
+            (result) => {
+                console.log(result.text);
+                alert('Mesaje enviado con exito');
+            },
+            (error) => {
+                console.log(error.text);
+                console.log('Error');
+                alert('Error al enviar el mensaje');
+            }
+        );
+        e.target.reset();
+    };
+
     return (
         <>
             <h1>Contacto</h1>
             <div>
-                <form>
+                <form id='contact-form' onSubmit={handleOnSubmit}>
                     <div className='form-item'>
                         <input
                             type='text'
-                            name='user_name'
+                            id='from_name'
+                            name='from_name'
                             placeholder='Nombre'
                         />
                     </div>
                     <div className='form-item'>
                         <input
                             type='email'
-                            id='email'
-                            name='user_email'
+                            id='from_email'
+                            name='from_email'
                             placeholder='name@example.com'
                         />
                     </div>
                     <div className='form-textarea'>
                         <textarea
                             type='text'
+                            id='message'
                             name='message'
                             placeholder='Mensaje'
                         />
                     </div>
                     <input type='hidden' name='form_submission' value='1' />
                     <button type='submit' class='submit-button'>
-                        <p>
-                            <span class='material-symbols-outlined'>send</span>
-                            Enviar
-                        </p>
+                        <span class='material-symbols-outlined'>send</span>
+                        Enviar
                     </button>
                 </form>
             </div>
