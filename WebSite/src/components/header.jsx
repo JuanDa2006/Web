925c34a0d43bd587be842e2264e947c4dbe61733
JuanDa2006 <86136478+JuanDa2006@users.noreply.github.com>
2024-05-03 09:11:01 +0200

    Up

--- WebSite/src/components/header.jsx
+++ WebSite/src/components/header.jsx
@@ -4,13 +4,13 @@ export default function Header() {
     return (
         <>
             <header>
-                <div className="header-text">
+                <div className='header-text'>
                     <h1>Genios Web</h1>
                     <p>Navegando a tu Éxito Digital</p>
                 </div>
-                <div className="header-img">
-					<img src={banner}/>
-				</div>
+                <div className='header-img'>
+                    <img src={banner} />
+                </div>
             </header>
         </>
     );
